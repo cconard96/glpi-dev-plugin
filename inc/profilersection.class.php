ce8349c8c423124125bc1261231a485f8e2455b2
Curtis Conard <cconard96@gmail.com>
2022-12-12 13:29:28 -0500

    improved profiler

--- inc/profilersection.class.php
+++ inc/profilersection.class.php
@@ -1,9 +1,16 @@
 <?php
 
+use Ramsey\Uuid\Uuid;
+
 class PluginDevProfilerSection {
 
    /** @var string */
    private $category;
+
+    /** @var string */
+    private $name;
+
+    private $session_id;
 
    /** @var int */
    private $start;
@@ -11,13 +18,17 @@ class PluginDevProfilerSection {
    /** @var int */
    private $end;
 
-   public function __construct(string $category, int $start) {
+   private $level = PluginDevProfiler::LEVEL_INFO;
+
+   public function __construct(string $category, string $name, $start) {
       $this->category = $category;
-      $this->start = $start;
+      $this->name = $name;
+      $this->session_id = session_id() ?? (string) Uuid::uuid4();
+      $this->start = (int) $start;
    }
 
-   public function end(int $time): void {
-      $this->end = $time;
+   public function end($time): void {
+      $this->end = (int) $time;
    }
 
    public function getStart(): int {
@@ -32,12 +43,51 @@ class PluginDevProfilerSection {
       return $this->category;
    }
 
+   public function getName(): string {
+      return $this->name;
+   }
+
    public function getDuration(): int {
       $end = $this->end ?? (int) (microtime(true) * 1000);
-      return $end- $this->start;
+      return $end - $this->start;
    }
 
    public function isFinished(): bool {
       return $this->end !== null;
+   }
+
+   public function getLevel(): int {
+      return $this->level;
+   }
+
+   public function setLevel(int $level): void {
+      $this->level = $level;
+   }
+
+   public function getSessionID(): string {
+      return $this->session_id;
+   }
+
+   public function setSessionID(string $session_id): void {
+      $this->session_id = $session_id;
+   }
+
+   public function toArray(): array {
+        return [
+             'session_id' => $this->session_id,
+             'category' => $this->category,
+             'name'     => $this->name,
+             'start'    => $this->start,
+             'end'      => $this->end,
+             'level'    => $this->level,
+        ];
+   }
+
+   public static function fromArray(array $array): self {
+        $section = new self($array['category'], $array['name'], $array['start']);
+        $section->end($array['end']);
+        $section->setLevel($array['level']);
+        $section->setSessionID($array['session_id']);
+        return $section;
    }
 }
